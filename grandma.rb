511e965907e407d452fc3bfbbf0787c9e4e982d2
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-23 20:02:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speaking_to_grandma
   elsif phrase == uppercase
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    return I LOVE YOU TOO PUMPKIN!
+    return "I LOVE YOU TOO PUMPKIN!"
   else
     return false 
   end
