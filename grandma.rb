f96fa7492bc4473c1685a9a1898de3487cd5cd5a
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-23 20:14:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string = !string.upcase
+  if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif string = string.upcase
+  elsif string.upcase
     return "NO, NOT SINCE 1938!"
-  elsif string = "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
   end
-end
+end 
