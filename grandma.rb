4b264772f7465f0fc7ec09d6f63cdb32b5ff7f07
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-23 20:02:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,15 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speaking_to_grandma
+  if phrase == lowercase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == uppercase
+    return "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return I LOVE YOU TOO PUMPKIN!
+  else
+    return false 
+  end
+end
